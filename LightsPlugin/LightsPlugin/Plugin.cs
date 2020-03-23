d4ab7205400458916aedca8293fece876b813f70
Rin <m.r.eilert@st.hanze.nl>
2020-03-23 21:56:09 +0100

    Update Plugin.cs

--- LightsPlugin/LightsPlugin/Plugin.cs
+++ LightsPlugin/LightsPlugin/Plugin.cs
@@ -115,7 +115,7 @@ namespace Lights {
             // Broadcast
             AnnounceDuration = Config.GetUInt("lights_announce_duration", 10);
             DoAnnouncement = Config.GetBool("lights_announce", true);
-            Announcement = Config.GetString("lights_announce_text", "<color=aqua>Lights have been turned off by %player for %ss! SpooOOO��Ooooky!</color>");
+            Announcement = Config.GetString("lights_announce_text", "<color=aqua>Lights have been turned off by %player for %ss! SpooOOOººOoooky!</color>");
 
             // C.A.S.S.I.E.
             CassieAutoAnnouncement = Config.GetString("lights_cassie_announcement_auto", "generator .g3 automatic reactivation started .g3 .g4");
